a0db542e600173e6eeeb21a990e2c095f1108d1e
Txekala <81804127+txekala@users.noreply.github.com>
2021-06-02 13:51:18 +0200

    Update databaseConection.dart

--- lib/src/controller/databaseConection.dart
+++ lib/src/controller/databaseConection.dart
@@ -4,7 +4,7 @@ import 'package:mysql1/mysql1.dart';
 class databaseConection  {
 
    var settings = new ConnectionSettings(
-        host: 'localhost:3306',
+        host: '10.0.4.55',
         port: 3306,
         user: 'dev',
         password: 'P@ssw0rd1',
